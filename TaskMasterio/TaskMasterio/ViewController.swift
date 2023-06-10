dbbcbdc87ce29e2b36155c22eab755b0097b3468
Aynur19 <nasybullin.aynur@yandex.ru>
2023-06-10 23:11:09 +0300

    created project: Task Masterio;

---
+++ TaskMasterio/TaskMasterio/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskMasterio
+//
+//  Created by Aynur Nasybullin on 10.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
