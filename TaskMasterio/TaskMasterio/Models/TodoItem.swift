1889b771d707ec1e825a6559180a7e0c971d44ab
Aynur19 <nasybullin.aynur@yandex.ru>
2023-06-11 09:17:37 +0300

    added struct TodoItem;

--- TaskMasterio/TaskMasterio/Models/TodoItem.swift
+++ TaskMasterio/TaskMasterio/Models/TodoItem.swift
@@ -13,4 +13,23 @@ enum Priority {
     case low
 }
 
-
+struct TodoItem {
+    let id: String
+    let text: String
+    let priority: Priority
+    let deadline: Date?
+    let isDone: Bool
+    let createdOn: Date
+    let updatedOn: Date?
+    
+    init(id: String = UUID().uuidString, text: String, priority: Priority, deadline: Date, isDone: Bool,
+         createdOn: Date, updatedOn: Date?) {
+        self.id = id
+        self.text = text
+        self.priority = priority
+        self.deadline = deadline
+        self.isDone = isDone
+        self.createdOn = createdOn
+        self.updatedOn = updatedOn
+    }
+}
