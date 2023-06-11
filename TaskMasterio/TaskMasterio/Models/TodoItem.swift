399cc4e571819995d4e7f943ee9701f0076694d0
Aynur19 <nasybullin.aynur@yandex.ru>
2023-06-11 09:11:36 +0300

    added enum Priority for TodoItem;

---
+++ TaskMasterio/TaskMasterio/Models/TodoItem.swift
@@ -0,0 +1,16 @@
+//
+//  TodoItem.swift
+//  TaskMasterio
+//
+//  Created by Aynur Nasybullin on 11.06.2023.
+//
+
+import Foundation
+
+enum Priority {
+    case high
+    case medium
+    case low
+}
+
+
