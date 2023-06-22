cb8311d9941545490edf5889cd54ce0c0c4668b0
Aynur19 <nasybullin.aynur@yandex.ru>
2023-06-22 11:09:58 +0300

    added test button in start view controller;

--- TaskMasterio/TaskMasterio/ViewController.swift
+++ TaskMasterio/TaskMasterio/ViewController.swift
@@ -11,9 +11,31 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .white
+        
+        addTestButton()
     }
 
-
+    private func addTestButton() {
+        let button = UIButton(type: .system)
+        button.setTitle("Show Task View", for: .normal)
+        button.addTarget(self, action: #selector(testButtonTapped), for: .touchUpInside)
+        
+        view.addSubview(button)
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            
+            button.widthAnchor.constraint(equalToConstant: 300),
+            button.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+    @objc func testButtonTapped() {
+        print("Button tapped!")
+    }
 }
 
