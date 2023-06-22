cdeeb331a54441bced98f75b46713b05fe7a5687
Aynur19 <nasybullin.aynur@yandex.ru>
2023-06-22 13:45:36 +0300

    added task description view in TodoItemViewController;

---
+++ TaskMasterio/TaskMasterio/Screens/TodoItemScrollView.swift
@@ -0,0 +1,14 @@
+//
+//  TodoItemScrollView.swift
+//  TaskMasterio
+//
+//  Created by Aynur Nasybullin on 22.06.2023.
+//
+
+import UIKit
+
+final class TodoItemScrollView: UIScrollView, UITextViewDelegate {
+    
+    
+    
+}
--- TaskMasterio/TaskMasterio/Screens/TodoItemViewController.swift
+++ TaskMasterio/TaskMasterio/Screens/TodoItemViewController.swift
@@ -7,9 +7,14 @@
 
 import UIKit
 
-final class TodoItemViewController: UIViewController {
+final class TodoItemViewController: UIViewController, UITextViewDelegate {
     
     let taskTitle = "Дело"
+    
+    private var scrollView: UIScrollView?
+    private var taskDescriptionView: UITextView?
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +23,7 @@ final class TodoItemViewController: UIViewController {
         view.backgroundColor = .systemBackground
         
         prepareNavigationBar()
+        prepareContent()
     }
     
     private func prepareNavigationBar() {
@@ -39,5 +45,60 @@ final class TodoItemViewController: UIViewController {
     @objc func cancelButtonTapped() { }
     
     @objc func saveButtonTapped() { }
+    
+    private func prepareContent() {
+        let scrollView = UIScrollView()
+        view.addSubview(scrollView)
+        
+        let taskDescriptionView = getTaskDescriptionView()
+        scrollView.addSubview(taskDescriptionView)
+        
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        taskDescriptionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+
+            taskDescriptionView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 16),
+            taskDescriptionView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -16),
+            taskDescriptionView.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 16),
+            taskDescriptionView.bottomAnchor.constraint(lessThanOrEqualTo: scrollView.bottomAnchor, constant: -16),
+          
+            taskDescriptionView.widthAnchor.constraint(equalTo: scrollView.widthAnchor, constant: -32),
+            taskDescriptionView.heightAnchor.constraint(greaterThanOrEqualToConstant: 120).with(priority: .defaultLow),
+        ])
+        
+        //taskDescriptionView.addObserver(self, forKeyPath: "text", options: .new, context: nil)
+
+        self.taskDescriptionView = taskDescriptionView
+        self.scrollView = scrollView
+        
+    }
+    
+    private func getTaskDescriptionView() -> UITextView {
+        let textView = UITextView()
+        
+        textView.font = UIFont.systemFont(ofSize: 17)
+        textView.tintColor = .black
+        textView.text = "Что надо сделать?"
+        textView.delegate = self
+        
+        return textView
+    }
+    
+//    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?,
+//                               context: UnsafeMutableRawPointer?) {
+//        if keyPath == "text", let textView = object as? UITextView {
+//            let contentSize = textView.contentSize
+//            let newHeight = contentSize.height > 120 ? contentSize.height : 120 // Minimum height
+//
+////            taskDescriptionView.heightAnchor.constraint(equalToConstant: newHeight)
+//            scrollView.layoutIfNeeded()
+//        }
+//    }
+
 }
 
