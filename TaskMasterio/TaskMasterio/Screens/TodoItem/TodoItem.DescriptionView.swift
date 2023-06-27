17582af43526afbf6414de09a228e2b3cab0e98a
Aynur19 <nasybullin.aynur@yandex.ru>
2023-06-27 21:49:56 +0300

    added description placeholder hiding;

--- TaskMasterio/TaskMasterio/Screens/TodoItem/TodoItem.DescriptionView.swift
+++ TaskMasterio/TaskMasterio/Screens/TodoItem/TodoItem.DescriptionView.swift
@@ -21,15 +21,32 @@ final class TodoItemDescriptionView: UITextView {
         setupPlaceHolderLabel()
     }
     
+    //    deinit {
+    //        NotificationCenter.default.removeObserver(self, name: UITextView.textDidChangeNotification, object: nil)
+    //    }
+    
+    override func becomeFirstResponder() -> Bool {
+        let didBecomeFirstResponder = super.becomeFirstResponder()
+        placeholderLabel.isHidden = true
+        return didBecomeFirstResponder
+    }
+    
+    override func resignFirstResponder() -> Bool {
+        let didResignFirstResponder = super.resignFirstResponder()
+        placeholderLabel.isHidden = !text.isEmpty
+        return didResignFirstResponder
+    }
+    
     private func setupPlaceHolderLabel() {
         addSubview(placeholderLabel)
         
-        placeholderLabel.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             placeholderLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: Sizes.marginH),
             placeholderLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -Sizes.marginH),
             placeholderLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: Sizes.marginV),
         ])
+        
+        //        NotificationCenter.default.addObserver(self, selector: #selector(textDidChange), name: UITextView.textDidChangeNotification, object: nil)
     }
     
     private lazy var placeholderLabel: UILabel = {
@@ -37,7 +54,19 @@ final class TodoItemDescriptionView: UITextView {
         label.tintColor = UIColor(named: AccentColors.backSecondary)
         label.textColor = UIColor(named: AccentColors.labelTertiary)
         label.text = Values.taskDescriptionPlacholder
+        label.translatesAutoresizingMaskIntoConstraints = false
         
         return label
     }()
+    
+    private var placeholder: String? {
+        get { return placeholderLabel.text }
+        set { placeholderLabel.text = newValue }
+    }
+    
+    //    @objc private func textDidChange() {
+    //        placeholderLabel.isHidden = !text.isEmpty
+    //    }
 }
+
+//extension TodoItemDescriptionView: UITex
