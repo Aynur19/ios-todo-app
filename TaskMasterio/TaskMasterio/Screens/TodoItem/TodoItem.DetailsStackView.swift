b0c55d8a9947a08e47aeaf206bf6d7f3c2f0674c
Aynur19 <nasybullin.aynur@yandex.ru>
2023-06-28 09:54:12 +0300

    added details stack view;

---
+++ TaskMasterio/TaskMasterio/Screens/TodoItem/TodoItem.DetailsStackView.swift
@@ -0,0 +1,34 @@
+//
+//  TodoItem.DetailsStackView.swift
+//  TaskMasterio
+//
+//  Created by Aynur Nasybullin on 27.06.2023.
+//
+
+import UIKit
+
+final class TodoItemDetailsStackView: UIStackView {
+    
+    // MARK: - Lifesycle Functions
+    init() {
+        super.init(frame: .zero)
+        
+        setupDetailsStackView()
+    }
+    
+    @available(*, unavailable)
+    required init(coder: NSCoder) {
+        fatalError("Trying to initialize Todo Item Details Stack View...")
+    }
+    
+    // MARK: - Setup Functions
+    private func setupDetailsStackView() {
+        self.axis = .vertical
+        self.alignment = .center
+        self.spacing = Sizes.margin_16
+        self.backgroundColor = UIColor(named: AccentColors.backSecondary)
+        self.layer.cornerRadius = Sizes.cornerRadius
+        self.translatesAutoresizingMaskIntoConstraints = false
+    }
+}
+
--- TaskMasterio/TaskMasterio/Screens/TodoItem/TodoItem.ScrollView.swift
+++ TaskMasterio/TaskMasterio/Screens/TodoItem/TodoItem.ScrollView.swift
@@ -28,37 +28,28 @@ final class TodoItemScrollView: UIScrollView {
     }
     
     private func setupDescriptionView() {
-        addSubview(descriptionView)
+        self.addSubview(descriptionView)
         
         NSLayoutConstraint.activate([
             descriptionView.centerXAnchor.constraint(equalTo: self.centerXAnchor),
-            descriptionView.widthAnchor.constraint(equalTo: self.widthAnchor, constant: Sizes.margin_2x16),
+            descriptionView.widthAnchor.constraint(equalTo: self.widthAnchor, constant: -Sizes.margin_2x16),
             descriptionView.topAnchor.constraint(equalTo: self.topAnchor),
             descriptionView.heightAnchor.constraint(greaterThanOrEqualToConstant: Sizes.descriptionMinHeight),
         ])
     }
     
+    
+    private func setupDetailsStackView() {
+        self.addSubview(detailsStackView)
+        
+        NSLayoutConstraint.activate([
+            detailsStackView.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            detailsStackView.widthAnchor.constraint(equalTo: self.widthAnchor, constant: -Sizes.margin_2x16),
+        ])
+    }
+    
     // MARK: - UI Elements
     private lazy var descriptionView = TodoItemDescriptionView()
+    
+    private lazy var detailsStackView = TodoItemDetailsStackView()
 }
-//    private func setupContentStackView() {
-//        addSubview(contentStackView)
-//
-//        NSLayoutConstraint.activate([
-//            contentStackView.centerXAnchor.constraint(equalTo: self.centerXAnchor),
-//            contentStackView.widthAnchor.constraint(equalTo: self.widthAnchor, constant: -Sizes.margin2xH),
-//            contentStackView.topAnchor.constraint(equalTo: self.topAnchor, constant: Sizes.spacingV),
-//            contentStackView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -Sizes.spacingV),
-//        ])
-//    }
-//
-//    private lazy var contentStackView: UIStackView = {
-//        let stackView = TodoItemContentStackView(frame: .zero)
-//        stackView.axis = .vertical
-//        stackView.spacing = Sizes.spacingV
-//        stackView.alignment = .center
-//        stackView.translatesAutoresizingMaskIntoConstraints = false
-//
-//        return stackView
-//    }()
-//}
