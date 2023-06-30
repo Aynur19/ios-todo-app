bad95da72569d1a38e1043444c6d42e9e73da5db
Aynur19 <nasybullin.aynur@yandex.ru>
2023-06-30 07:05:32 +0300

    added: view model data generation;

--- TaskMasterio/TaskMasterio/Screens/TodoList/TodoList.ViewModel.swift
+++ TaskMasterio/TaskMasterio/Screens/TodoList/TodoList.ViewModel.swift
@@ -22,10 +22,54 @@ final class TodoListViewModel: ObservableObject {
     }
     
     private func loadData() {
-        try? dataCache.load(name: "Tasks", as: .json)
+        try? dataCache.load(name: "Tasks 2", as: .json)
         
-        for _ in 1...5 {
-            tasks.append(TodoItemViewModel(dataCache.tasks.first, with: dataCache))
+        if dataCache.tasks.isEmpty {
+            var newTasks = generateTasks()
+            for task in newTasks {
+                dataCache.add(task)
+            }
         }
+        
+        for task in dataCache.tasks {
+            tasks.append(TodoItemViewModel(task, with: dataCache))
+        }
+    }
+    
+    
+    private func generateTasks() -> [TodoItem] {
+        var tasks = [TodoItem]()
+        
+        tasks.append(
+            TodoItem(text: "Позвонить",
+                     priority: .low))
+        tasks.append(
+            TodoItem(text: "Подготовить презентацию для клиента",
+                     priority: .low))
+        tasks.append(
+            TodoItem(text: "Провести анализ рынка и подготовить отчет с рекомендациями",
+                     priority: .low))
+        tasks.append(
+            TodoItem(text: "Разработать стратегию маркетинговой кампании, включающую целевую аудиторию, каналы продвижения и бюджет",
+                     priority: .low))
+        tasks.append(
+            TodoItem(text: "Организовать тренинг для сотрудников по повышению навыков коммуникации и эффективного управления временем",
+                     priority: .low))
+        tasks.append(TodoItem(text: "Отправить письмо",
+                     priority: .low))
+        tasks.append(
+            TodoItem(text: "Организовать встречу с партнерами",
+                     priority: .low))
+        tasks.append(
+            TodoItem(text: "Согласовать бюджет проекта с финансовым отделом и утвердить его",
+                     priority: .low))
+        tasks.append(
+            TodoItem(text: "Провести тестирование нового программного обеспечения и составить детальный отчет о найденных ошибках и рекомендациях по исправлению",
+                     priority: .low))
+        tasks.append(
+            TodoItem(text: "Провести аудит информационной безопасности компании и разработать план по устранению выявленных уязвимостей и рисков",
+                     priority: .low))
+        
+        return tasks
     }
 }
