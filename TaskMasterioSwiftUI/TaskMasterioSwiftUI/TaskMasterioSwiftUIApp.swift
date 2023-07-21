ced97b6d7fb027891161677457a275ca66bbef2e
Aynur19 <nasybullin.aynur@yandex.ru>
2023-07-21 16:11:30 +0300

    added: new project on SwiftUI;

---
+++ TaskMasterioSwiftUI/TaskMasterioSwiftUI/TaskMasterioSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskMasterioSwiftUIApp.swift
+//  TaskMasterioSwiftUI
+//
+//  Created by Aynur Nasybullin on 21.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct TaskMasterioSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
