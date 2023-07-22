9023e7f846ac5d75d682de4eb3578010c0ffd061
Aynur19 <nasybullin.aynur@yandex.ru>
2023-07-22 05:17:23 +0300

    added blue button;

--- TaskMasterioSwiftUI/TaskMasterioSwiftUI/ContentView.swift
+++ TaskMasterioSwiftUI/TaskMasterioSwiftUI/ContentView.swift
@@ -11,51 +11,65 @@ import SwiftUI
 
 struct ContentView: View {
     @ObservedObject var todoListVM: TodoListViewModel
-//    let todoList = MockData.todoList
+    //    let todoList = MockData.todoList
     
     var body: some View {
         NavigationView {
-            ScrollView(.vertical) {
-                
-                HStack {
-                    Text("Выполенено - \(todoListVM.completedCount)")
-                        .font(Fonts.subhead)
-                        .foregroundColor(Colors.labelTertiary)
+            ZStack(alignment: .centerLastTextBaseline) {
+                ScrollView(.vertical) {
                     
-                    Spacer(minLength: 16)
-                    
-                    Text(todoListVM.withCompleted ? "Скрыть" : "Показать")
-                        .font(Fonts.subheadBold)
-                        .foregroundColor(Colors.blue)
-                        .onTapGesture {
-                            todoListVM.withCompleted.toggle()
-                        }
-                }
-                .padding(.horizontal, 16)
-                
-                LazyVStack(alignment: .leading) {
-                    ForEach(todoListVM.shownList, id: \.id) { item in
-                        TodoItemCellView(todoItemVM: item)
+                    HStack {
+                        Text("Выполенено - \(todoListVM.completedCount)")
+                            .font(Fonts.subhead)
+                            .foregroundColor(Colors.labelTertiary)
                         
-                        Divider()
-                            .foregroundColor(Colors.supportSeparator)
-                            .frame(height: 1)
-                            .padding(.leading, 52)
+                        Spacer(minLength: 16)
+                        
+                        Text(todoListVM.withCompleted ? "Скрыть" : "Показать")
+                            .font(Fonts.subheadBold)
+                            .foregroundColor(Colors.blue)
+                            .onTapGesture {
+                                todoListVM.withCompleted.toggle()
+                            }
                     }
+                    .padding(.horizontal, 16)
                     
-                    TodoItemLastCellView()
-                        .frame(minHeight: 56)
+                    LazyVStack(alignment: .leading) {
+                        ForEach(todoListVM.shownList, id: \.id) { item in
+                            TodoItemCellView(todoItemVM: item)
+                            
+                            Divider()
+                                .foregroundColor(Colors.supportSeparator)
+                                .frame(height: 1)
+                                .padding(.leading, 52)
+                        }
+                        
+                        TodoItemLastCellView()
+                            .frame(minHeight: 56)
+                    }
+                    .background(Colors.backSecondary)
+                    .cornerRadius(16)
+                    .frame(maxWidth: .infinity)
                 }
-                .background(Colors.backSecondary)
+                
                 .cornerRadius(16)
+                .padding(16)
                 .frame(maxWidth: .infinity)
+                .background(Colors.backPrimary)
+                .navigationTitle("Мои дела")
+                
+                
+                Button(action: {
+                    // Действия при нажатии кнопки
+                    print("Кнопка нажата!")
+                }) {
+                    Image("Add Task")
+                        .frame(width: 44, height: 44, alignment: .center)
+                }
+                .padding(.bottom, 20)
             }
             
-            .cornerRadius(16)
-            .padding(16)
-            .frame(maxWidth: .infinity)
-            .background(Colors.backPrimary)
-            .navigationTitle("Мои дела")
+           
         }
         .background(Colors.backPrimary)
     }
